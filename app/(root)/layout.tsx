2b90957956e940d7f9003618953a618f43df5e75
Bipul Mishra <bipulm946@gmail.com>
2024-05-28 12:19:03 +0530

    Implement auth,appwrite, and sentry

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,27 +1,31 @@
-import Image from "next/image";
-import Sidebar from "@/components/Sidebar" ;
 import MobileNav from "@/components/MobileNav";
-export default function RootLayout({
+import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn ={firstName:'Bipul' ,lastName:'Mishra'};
-  return (
-     <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn}/>
-        <div className="flex size-full flex-col">
-          <div className="root-layout">
-            <Image src="/icons/logo.svg" width={30}
-            height={30} alt="logo"/>
-            <div>
-              <MobileNav user={loggedIn}/>
-            </div>
+  const loggedIn = await getLoggedInUser();
 
+  if(!loggedIn) redirect('/sign-in')
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
           </div>
-          {children}
         </div>
-        
-     </main>
+        {children}
+      </div>
+    </main>
   );
 }
